5a9375ae47179e636914e771b283c4d36b26de5f
fanxx-xhw <2758074764@qq.com>
2018-09-10 10:41:50 +0800

    新增readme

--- bin/main.py
+++ bin/main.py
@@ -1,10 +1,11 @@
 import unittest
 from lib.path import WEBCASEPATH,REPORTPATH
 from lib.HTMLTestRunner import HTMLTestRunner
-from lib.tool import Tool
+from lib.tool import T
+import datetime
 class Main(object):
     def run(self):
-        Tool().clear_picture()
+        T.clear_picture()
         suit = unittest.TestSuite()
         cases = unittest.defaultTestLoader.discover(WEBCASEPATH)
         for case in cases:
@@ -14,5 +15,7 @@ class Main(object):
         runner.run(suit)
         f.flush()
         f.close()
+        T.send_email('2758074764@qq.com','fanxiangxiang@huikecloud.com','auto_test_report','日期：%s'%datetime.datetime.today().strftime('%Y%m%d%H%M%S'),r'G:\xinhuiwen\report\report.html')
+
 if __name__ == '__main__':
     Main().run()
